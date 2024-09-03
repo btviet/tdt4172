bf493dc42081001c9fa5c849d5cd0878c8f6207b
btviet <vietshay@gmail.com>
2024-09-03 16:42:58 +0200

    implement LR

--- linear_regression.py
+++ linear_regression.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class LinearRegression():
     
-    def __init__(self, learning_rate=0.00001, epochs=40):
+    def __init__(self, learning_rate=0.0001, epochs=40):
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.weights, self.bias = None, None
@@ -16,15 +16,18 @@ class LinearRegression():
         Y_pred: The prediction of the target variable
         """
         n = X.shape[0]
-        grad_w = -2*np.dot(X.T, Y-Y_pred)/n
-        grad_b = -2*(Y-Y_pred)/n
+        #grad_w = -2*np.dot(X.T, Y_pred-Y)/n
+        #grad_b = -2*(np.sum(Y_pred-Y))/n
+
+        grad_w = - ( 2 * ( X.T ).dot( Y - Y_pred )  ) / n
+       
+        grad_b = - 2 * np.sum( Y - Y_pred ) / n
+
         return grad_w, grad_b
     
     def MSE(self, Y, Y_pred):
-        n = np.shape(Y)
-        return (Y-Y_pred)/n
-    
-
+        return (np.square(Y - Y_pred)).mean()
+        
     def fit(self, X, Y):
         """
         Estimates parameters for the classifier
@@ -34,16 +37,19 @@ class LinearRegression():
                 n rows (#samples) and m columns (#features)
             Y (array<n>): a vector of floats. The observed target variables
         """
-        n = X.shape[0]
-        self.weights = np.zeros([n])
+        n = X.shape[0] # Number of data points
+        
+        self.weights = np.zeros(1) # np.zeros(n)
+        print("self.weights: ", self.weights)
         self.bias = 0
         for i in range(self.epochs):
-            Y_pred = np.dot(X, self.weights) + self.bias
+            Y_pred = self.predict( X ) 
             grad_w, grad_b = self.gradient_descent(X, Y, Y_pred)  
             self.weights = self.weights - self.learning_rate*grad_w
             self.bias = self.bias - self.learning_rate*grad_b
             current_loss = self.MSE(Y, Y_pred)  
             print(f'Epoch: {i}. Current loss: {current_loss}')
+            print("bias: ", self.bias, "self.weights: ", self.weights[0])
     def predict(self, X):
         """
         Generates predictions
@@ -57,8 +63,9 @@ class LinearRegression():
         Returns:
             A length m array of floats
         """
-        # TODO: Implement
-        raise NotImplementedError("The predict method is not implemented yet.")
+        return X*self.weights+ self.bias
+        #return np.dot(X, self.weights) + self.bias
+        
 
 import pandas as pd
 data = pd.read_csv('mission1.csv')
